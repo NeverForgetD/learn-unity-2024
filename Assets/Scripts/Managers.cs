1bee8f6e1674a8e2bf779201d20b80fd90d42a82
NeverForgetD <0327max@gmail.com>
2024-09-26 20:08:31 +0900

    Maangers (singleton)

--- Assets/Scripts/Managers.cs
+++ Assets/Scripts/Managers.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Managers : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    static Managers s_instance;
+    public static Managers Instance { get { return s_instance; } }
+
+
+    private void Start()
     {
-        
+        Init();
     }
 
-    // Update is called once per frame
-    void Update()
+    static void Init()
     {
-        
+        if (s_instance == null)
+        {
+            GameObject go = GameObject.Find("@Managers");
+            if (go == null)
+            {
+                go = new GameObject { name = "@Managers" };
+                go.AddComponent<Managers>();
+            }
+
+            DontDestroyOnLoad(go);
+            s_instance = go.GetComponent<Managers>();
+        }
     }
 }
